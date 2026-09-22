480129f3ac2e78d3b0c069439a73e153b537cf85
Térence FERUT <46047190+AstrowareConception@users.noreply.github.com>
2026-09-22 22:34:04 +0200

    Sécuriser l'exemple de balise PHP erronée

--- exercices/00-premiers-pas/starter/erreurs.php
+++ exercices/00-premiers-pas/starter/erreurs.php
@@ -4,7 +4,6 @@
 // Fragment B : $titre = "Alien;
 // Fragment C : echo $annee; $annee = 1979;
 // Fragment D : $message = "Film : " + "Alien";
-// Fragment E : <? php echo "Bienvenue"; ?>
+// Fragment E : &lt;? php echo "Bienvenue"; ?&gt;
 
 echo "Recopiez et testez un seul fragment à la fois dans un fichier temporaire.\n";
-
